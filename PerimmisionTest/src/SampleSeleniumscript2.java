41bb0b6166b6ded14bc10701cf9484478b9616f7
rgopavarapu <rgopavarapu@nu.edu>
2023-07-05 07:46:04 -0700

    Commit

--- PerimmisionTest/src/SampleSeleniumscript2.java
+++ PerimmisionTest/src/SampleSeleniumscript2.java
@@ -7,8 +7,7 @@ public class SampleSeleniumscript2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.setProperty("webdriver.chrome.driver", "C:\\Rajesh\\Java-Eclipse\\chromedriver.exe Test Rajesh");
-		System.setProperty("webdriver.chrome.driver", "C:\\Rajesh\\Java-Eclipse\\chromedriver.exe Test Rajesh");
+		System.out.println("Code change to");
 		}
 }
  
