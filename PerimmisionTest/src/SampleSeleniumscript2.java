2d90a72f4278e7570048d9d2d8cf93454a8ba68f
rajesh5400 <30725479+rajesh5400@users.noreply.github.com>
2023-07-05 19:26:42 +0530

    Update SampleSeleniumscript2.java

--- PerimmisionTest/src/SampleSeleniumscript2.java
+++ PerimmisionTest/src/SampleSeleniumscript2.java
@@ -9,7 +9,6 @@ public class SampleSeleniumscript2 {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\Rajesh\\Java-Eclipse\\chromedriver.exe Test Rajesh");
 		System.setProperty("webdriver.chrome.driver", "C:\\Rajesh\\Java-Eclipse\\chromedriver.exe Test Rajesh");
-	Systems.out.println("Code change in Github")
-}
+	}
 
 }
